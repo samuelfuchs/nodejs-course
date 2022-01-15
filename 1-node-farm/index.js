2d3db088305c89582df82a56e73c5c37386accd3
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-15 05:02:02 -0300

    basic Routing implemented

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,34 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
+//////////////////////////////////
+// Routing
+const server = http.createServer((req, res) => {
+  const pathName = req.url
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW')
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT')
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    })
+    res.end('<h1>Page not found!</h1>')
+  }
+
+  return res.end('Hello from the Server!')
+})
+
+// Listening for incomming requests:
+// Starting up the server...
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000')
+})
+
+/*
 //////////////////////////////////
 // Server
 const server = http.createServer((req, res) => {
@@ -12,7 +40,7 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000')
 })
-
+*/
 //////////////////////////////////
 // Files
 /*

94cc1506c1aaa7aaa2bf6b7ffed8331fef00397a
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-15 04:44:20 -0300

    simple web server created using createServer()

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,20 @@
 const fs = require('fs')
+const http = require('http')
 
+//////////////////////////////////
+// Server
+const server = http.createServer((req, res) => {
+  res.end('Hello from the Server!')
+})
+
+// Listening for incomming requests:
+// Starting up the server...
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000')
+})
+
+//////////////////////////////////
+// Files
 /*
 // Blocking, synchronous way
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -9,6 +24,7 @@ fs.writeFileSync('./txt/output.txt', textOut)
 console.log('File written!')
 */
 
+/*
 // Non-blocking, asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
   if (err) return console.log('ERROR! 😭')
@@ -18,10 +34,10 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
       console.log(data3)
 
       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        //
         console.log('Your file has been written 😁')
       })
     })
   })
 })
 console.log('Will read file!')
+*/

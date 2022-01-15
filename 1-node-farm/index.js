0ba4d1c7152415008a984351d4869485e4f72744
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-15 04:32:04 -0300

    error handling example

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -11,6 +11,7 @@ console.log('File written!')
 
 // Non-blocking, asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('ERROR! 😭')
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     console.log(data2)
     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {

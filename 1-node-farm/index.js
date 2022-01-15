7d243898ce4313a7a1dd3beb37a4043042ff9f6a
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-15 16:52:28 -0300

    Building a (Very) Simple API

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,7 +3,10 @@ const http = require('http')
 const url = require('url')
 
 //////////////////////////////////
-// Routing
+// Building a (Very) Simple API
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
+
 const server = http.createServer((req, res) => {
   const pathName = req.url
 
@@ -11,6 +14,9 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW')
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT')
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' })
+    res.end(data)
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
@@ -18,8 +24,6 @@ const server = http.createServer((req, res) => {
     })
     res.end('<h1>Page not found!</h1>')
   }
-
-  return res.end('Hello from the Server!')
 })
 
 // Listening for incomming requests:

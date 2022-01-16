1058f43293f69875818df57ffb39b1a3af749d0b
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-16 10:41:36 -0300

    Parsing Variables from URLs

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -37,12 +37,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-  console.log(url.parse(req.url, true))
-  const pathName = req.url
+  const { query, pathname } = url.parse(req.url, true)
 
   // Overview page
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' })
 
     const cardsHtml = dataObj
@@ -53,11 +51,15 @@ const server = http.createServer((req, res) => {
     res.end(output)
 
     // Product page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT')
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' })
+    const product = dataObj[query.id]
+    const output = replaceTemplate(tempProduct, product)
+
+    res.end(output)
 
     // API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' })
     res.end(data)
 

b814d452dfb20b3667cac8b045de321e8fb972d1
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-16 08:15:51 -0300

    Parsing Variables from URLs part 1

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -37,6 +37,8 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
+  console.log(req.url)
+  console.log(url.parse(req.url, true))
   const pathName = req.url
 
   // Overview page

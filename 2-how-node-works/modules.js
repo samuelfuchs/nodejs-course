5f5a236081dfbd8ec617a131ef33aa4955c3cb9e
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-20 18:39:57 -0300

    Requiring Modules in Practice part 3

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -10,3 +10,8 @@ console.log(calc1.add(2, 5));
 // const calc2 = require('./test-module-2');
 const { add, multiply } = require('./test-module-2');
 console.log(multiply(2, 5));
+
+// caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();

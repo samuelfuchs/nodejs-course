93b733e6456dbd22cc1d9eb9c394b8aefa5bff67
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-20 18:37:32 -0300

    Requiring Modules in Practice part 2

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -7,5 +7,6 @@ const calc1 = new C();
 console.log(calc1.add(2, 5));
 
 //exports
-const calc2 = require('./test-module-2');
-console.log(calc2.multiply(2, 5));
+// const calc2 = require('./test-module-2');
+const { add, multiply } = require('./test-module-2');
+console.log(multiply(2, 5));

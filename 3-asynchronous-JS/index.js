120e4e97d2b7c24e5dc4160854a516c64089b141
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-20 19:01:39 -0300

    From Callback Hell to Promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -6,14 +6,15 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body);
+    .then((res) => {
+      console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
         if (err) return console.log(err.message);
-
         console.log('Random dog image saved to file');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });

11fcf6a66dc484293b4cf7e21a4cffda30ae1986
samuelfuchs <fuchsgerson@gmail.com>
2022-01-23 08:06:33 -0300

    Returning Values from Async Functions

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -33,9 +33,30 @@ const getDogPic = async () => {
     console.log('Random dog image saved to file');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+  return '2: READY :)';
 };
-getDogPic();
+
+(async () => {
+  try {
+    const x = await getDogPic();
+    console.log('x: ', x);
+    console.log('3: Done getting dog pics!');
+  } catch (err) {
+    console.log('ERROR !!!');
+  }
+})();
+
+// console.log('1: Will get dog pics!');
+// getDogPic()
+//   .then((x) => {
+//     console.log('x: ', x);
+//     console.log('3: Done getting dog pics!');
+//   })
+//   .catch((err) => {
+//     console.log('ERROR !!!');
+//   });
 
 // readFilePro(`${__dirname}/dog.txt`)
 //   .then((data) => {
